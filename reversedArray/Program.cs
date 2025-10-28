3b87fc3b0b6cd4d1adb8cb869b8fc5a1344e294e
twizzyy1 <kirillgeyer1102@gmail.com>
2025-10-28 18:06:27 +0300

    Add files via upload

---
+++ reversedArray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ArrayReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] originalArray = { 10, 20, 30, 40, 50 };
+
+            Console.WriteLine("Исходный массив: ");
+            for (int i = 0; i < originalArray.Length; i++)
+            {
+                Console.WriteLine(originalArray[i] + " ");
+            }
+
+            int[] reversedArray = new int[originalArray.Length];
+
+            for (int i = 0; i < originalArray.Length; i++)
+            {
+                reversedArray[i] = originalArray[originalArray.Length - 1 - i];
+            }
+
+            Console.WriteLine("Перевернутый массив: ");
+            for (int i = 0; i < reversedArray.Length; i++)
+            {
+                Console.WriteLine(reversedArray[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
