bb0b9e2a26440ae8dc62a7501714e11f6ce13fdd
twizzyy1 <kirillgeyer1102@gmail.com>
2025-11-25 16:11:27 +0300

    Add files via upload

---
+++ StringFormatting(10)/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+
+namespace StringFormatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Форматирование списка пользователей ---\n");
+
+            string inputString = "  иванов иван,петров петр, сидорова Анна, бобров БОРИС  ";
+
+            Console.WriteLine($"Исходная строка: \"{inputString}\"\n");
+
+            string formattedList = FormatUserList(inputString);
+
+            Console.WriteLine("Отформатированный список:");
+            Console.WriteLine(formattedList);
+
+            Console.ReadKey();
+        }
+
+        static string FormatUserList(string input)
+        { 
+
+            string cleanedInput = input.Trim();
+            string[] namePairs = cleanedInput.Split(',');
+
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < namePairs.Length; i++)
+            {
+                string cleanedPair = namePairs[i].Trim();
+
+                string[] parts = cleanedPair.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (parts.Length >= 2)
+                {
+                    string lastName = FormatName(parts[0]);
+
+                    string firstName = FormatName(parts[1]);
+
+                    result.AppendLine($"{i + 1}. {lastName} {firstName}");
+                }
+            }
+
+            return result.ToString();
+        }
+
+        static string FormatName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                return name;
+
+            string lowerCaseName = name.ToLower();
+
+            return char.ToUpper(lowerCaseName[0]) + lowerCaseName.Substring(1);
+        }
+    }
+}
